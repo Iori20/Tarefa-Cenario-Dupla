c0de29e10227bbb24ee081e9221ed6c83ca2bdde
iori20 <iori.leite@etec.sp.gov.br>
2021-09-20 18:49:31 -0300

    Tentativa

---
+++ Tarefa Cenários Dupla/Tarefa Cenários Dupla/Assets/Mover.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+
+    CharacterController controller;
+
+    Vector3 forward;
+    Vector3 strafe;
+    Vector3 vertical;
+
+    float forwardSpeed = 3f;
+    float strafeSpeed = 3f;
+
+    float gravity;
+    float jumpSpeed;
+    float maxJumpHeight = 2f;
+    float timeToMaxHeight = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        controller = GetComponent<CharacterController>();
+
+        gravity = (-2 * maxJumpHeight) / (timeToMaxHeight * timeToMaxHeight);
+        jumpSpeed = (2 * maxJumpHeight) / (timeToMaxHeight * timeToMaxHeight);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float forwardInput = Input.GetAxisRaw("Vertical");
+        float strafeInput = Input.GetAxisRaw("Horizontal");
+
+        forward = forwardInput * forwardSpeed * transform.forward;
+        strafe = strafeInput * strafeSpeed * transform.right;
+
+        vertical += gravity * Time.deltaTime * Vector3.up;
+
+        if(controller.isGrounded)
+        {
+            vertical = Vector3.down;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && controller.isGrounded)
+        {
+            vertical = jumpSpeed * Vector3.up;
+        }
+
+        Vector3 finalVelocity = forward + strafe + vertical;
+
+        controller.Move(finalVelocity * Time.deltaTime);
+    }
+}
